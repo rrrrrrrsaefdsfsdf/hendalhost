0dbf65fe4fbed06effff0fa867578f71cc148b0f
dev_mrx <dev@dev.com>
2025-09-01 20:36:04 +0300

    t

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-docker-compose up -d web
-docker-compose run --rm certbot certonly --standalone -d sweets.promo --non-interactive --agree-tos --email memememe@gmail.com
-docker-compose down
-docker-compose up -d
+# Start web and nginx services
+docker-compose up -d web nginx
+
+# Run certbot with webroot to generate certificates
+docker-compose run --rm certbot certonly --webroot -w /var/lib/letsencrypt -d sweets.promo --non-interactive --agree-tos --email memememe@gmail.com
+
+# Restart nginx to load the certificates
+docker-compose restart nginx
+
+# Start the certbot renewal service
+docker-compose up -d certbot
