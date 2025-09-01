3a148ddc5f1e71ce0ec237633a4c1a3a95619032
dev_mrx <dev@dev.com>
2025-09-01 21:31:49 +0300

    f

--- init-letsencrypt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker compose down
-docker compose run --rm certbot certonly --standalone -d sweets.promo --non-interactive --agree-tos --email memememe@gmail.com
-docker compose up -d
