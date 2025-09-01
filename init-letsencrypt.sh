27bf7d1a28f87bfda7814dce6c82a38b9e627798
dev_mrx <dev@dev.com>
2025-09-01 20:49:06 +0300

    a

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Stop all services to free up port 80
 docker compose down
-
-# Run certbot to generate certificates
 docker compose run --rm certbot certonly --standalone -d sweets.promo --non-interactive --agree-tos --email memememe@gmail.com
-
-# Start all services
 docker compose up -d
